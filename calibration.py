7edc05ce63903b5db83600a72b1cc030bcdde6ae
martinellop <pietromartinello.dev@gmail.com>
2024-02-05 20:19:37 +0100

    refactor calibration script

--- calibration.py
+++ calibration.py
@@ -1,22 +1,73 @@
 import cv2
+import ipaddress
+import argparse
+import os
+
+
+def get_arguments():
+    """Parse all the arguments provided from the CLI.
+    Returns:
+      A list of parsed arguments.
+    """
+    parser = argparse.ArgumentParser()
+
+
+    parser.add_argument("--source", "-s", type=str, required=True, help="Source of the video. It can be an IP, a path to a device, an index.")
+    parser.add_argument("--port", "-p", type=int, default=8080, help="In case of source from IP, you can set here the port. Default to 8080.")
+
+    parser.add_argument("--dst", "-d", type=str, required=True, help="Destination directory where to store pictures. If it doesn't exist yet, it will be created.")
+
+
+    return parser.parse_args()
+
+
+def is_ipv4(string):
+    try:
+        ipaddress.IPv4Network(string)
+        return True
+    except ValueError:
+        return False
+
+
 
 def save_frame(event, x, y, flags, param):
     global frame_counter
+    global dst_folder
     if event == cv2.EVENT_LBUTTONDOWN:
-        frame_name = f'mi8/frame_{frame_counter}.png'
-        cv2.imwrite(frame_name, frame)
-        print(f'{frame_name}')
-        frame_counter += 1
+        frame_name = os.path.join(dst_folder, f'frame_{frame_counter}.png')
+        res =  cv2.imwrite(frame_name, frame)
+        if res:
+            print(f'Stored {frame_name}')
+            frame_counter += 1
+        else:
+            print(f'Impossible to store {frame_name}')
+        
 
-ip_camera = '192.168.1.199'
-# Inserisci l'URL dello stream web
-url = f'http://{ip_camera}:8080/video'
 
-# Inizializza il video capture
-capture = cv2.VideoCapture(url)
 
+
+args = get_arguments()
+video_source = str(args.source)
+dst_folder = str(args.dst)
+
+if is_ipv4(video_source):
+    ip_port = int(args.source)
+    source = f'http://{video_source}:{ip_port}/video'
+else:
+    try:
+        source = int(video_source)
+    except:
+        source = video_source
+
+
+
+capture = cv2.VideoCapture(source)
 # Inizializza il contatore dei frame
 frame_counter = 0
+
+
+if not os.path.isdir(dst_folder):
+    os.mkdir(dst_folder)
 
 # Crea una finestra per visualizzare lo stream
 cv2.namedWindow("Web Stream")
